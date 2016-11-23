e8cb582f11e0a726b42724a8a404e9cbac2e32b3
Junior Conte <juniorcnt@gmail.com>
2016-11-22 23:47:21 -0200

    first commit

---
+++ app/scripts/app.js
@@ -0,0 +1,32 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name randlistApp
+ * @description
+ * # randlistApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('randlistApp', [
+    'ngAnimate',
+    'ngRoute',
+    'ngSanitize'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name randlistApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the randlistApp
+ */
+angular.module('randlistApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
