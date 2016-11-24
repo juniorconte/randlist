d08402da58ecf91300007d690d9208a5c64bd3f5
Junior Conte <juniorcnt@gmail.com>
2016-11-24 21:46:56 -0200

    ADD: Diretiva para ler conteúdo do arquivo selecionado no imput file

---
+++ test/spec/directives/onreadfile.js
@@ -0,0 +1,14 @@
+'use strict';
+
+describe('Directive: onReadFile', function () {
+
+  // load the directive's module
+  beforeEach(module('randlistApp'));
+
+  var scope;
+
+  beforeEach(inject(function ($rootScope) {
+    scope = $rootScope.$new();
+  }));
+
+});
