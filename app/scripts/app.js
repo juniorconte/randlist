53ab0b04e8daa1fcaadaf21960f0d1f5a86325df
Junior Conte <juniorcnt@gmail.com>
2016-11-24 19:32:58 -0200

    DEL: Conteúdo demonstrativo do Yeoman Generator

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -17,14 +17,7 @@ angular
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl',
-        controllerAs: 'main'
       })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl',
-        controllerAs: 'about'
       })
       .otherwise({
         redirectTo: '/'
