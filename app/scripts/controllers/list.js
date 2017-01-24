5a8c487484f3631c0a09d4d2b1bdad8bb508d331
Junior Conte <juniorcnt@gmail.com>
2017-01-24 20:21:30 -0200

    Utilização de separador com valor padrão

--- app/scripts/controllers/list.js
+++ app/scripts/controllers/list.js
@@ -12,38 +12,31 @@ angular.module('randlistApp')
 
     var list = this;
 
-	  list.csv = null
-
     function load() {
       list.head = localStorageService.get('head') || [];
       list.body = localStorageService.get('body') || [];
     }
 
-    list.preproccess = function (csv) {
-      list.csv = csv
-    }
+    list.proccess = function(csv, separator) {
+      var table = csv.split('\n').map(function(row, index) {
+        var content = {
+          data: row.split(separator || ','),
+          control: {}
+        };
 
-    list.proccess = function proccess(csv) {
-      var sep = $('#sep').val()
-      var table = list.csv.split('\n').map(function(row, index) {
-          var content = {
-            data: row.split(sep),
-            control: {}
-          };
+        if (!!content.data.toString() && index) {
+          content.control.index = index;
+          content.control.win = false;
+          content.control.winAt = null;
+        }
 
-          if (!!content.data.toString() && index) {
-            content.control.index = index;
-            content.control.win = false;
-            content.control.winAt = null;
-          }
-
-          return content;
-        })
-        .filter(function(row) {
-          return row.data.some(function(collum) {
-            return !!collum;
-          });
+        return content;
+      })
+      .filter(function(row) {
+        return row.data.some(function(collum) {
+          return !!collum;
         });
+      });
 
       localStorageService.set('head', table.shift());
       localStorageService.set('body', table);
@@ -52,7 +45,6 @@ angular.module('randlistApp')
 
     list.clean = function clean() {
       if ($window.confirm('Isso apagará toda a lista, deseja continuar?')) {
-        list.csv = undefined
         localStorageService.clearAll();
         load();
       }
